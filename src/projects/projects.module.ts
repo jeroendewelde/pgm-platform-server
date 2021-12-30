6f066fdfd2aaee0abb228ec971f0f6ed74164deb
pgm-jeroendewelde <Jeroen.Dewelde@student.arteveldehs.be>
2021-12-30 02:59:17 +0100

    closes #17; added array of strings as tags to courses

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -4,7 +4,6 @@ import { ProjectsResolver } from './projects.resolver';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from './entities/project.entity';
 import { CoursesModule } from 'src/courses/courses.module';
-import { PersonsService } from 'src/persons/persons.service';
 import { PersonsModule } from 'src/persons/persons.module';
 import { AttachmentsModule } from 'src/attachments/attachments.module';
 
