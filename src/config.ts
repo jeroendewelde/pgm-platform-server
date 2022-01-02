9f43a6c161ec398d9bfbcf77ee6f0e4d3adfab25
pgm-jeroendewelde <Jeroen.Dewelde@student.arteveldehs.be>
2022-01-02 17:29:20 +0100

    added Procfile

--- src/config.ts
+++ src/config.ts
@@ -10,6 +10,13 @@ export const config = () => ({
       autoLoadEntities: true,
       synchronize: true,
       // ssl: true,
+      
+        // ssl: {
+        //   require: true,
+        //   rejectUnauthorized: false
+        // }
+      
+      sslMode: 'require',
   },
   databaseDev: {  
       type: 'postgres',
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env);
+  // console.log(process.env);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT || 3000);
 }
