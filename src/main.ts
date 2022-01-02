8a2461894673b1a67704537ddc14125b5f77f30e
pgm-jeroendewelde <Jeroen.Dewelde@student.arteveldehs.be>
2022-01-02 17:51:48 +0100

    modified port conifguration

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // console.log(process.env);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
+  // await app.listen(process.env.PORT || 3000);
+  
+  await app.listen(3000);
 }
 bootstrap();
